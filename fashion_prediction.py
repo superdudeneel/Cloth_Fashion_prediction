2c22c2f2d9f87f9b01ef6eba541f2a77e877813c
superdudeneel <neelsane@gmail.com>
2025-03-26 12:04:54 +0530

    Update fashion_prediction.py

--- fashion_prediction.py
+++ fashion_prediction.py
@@ -6,10 +6,13 @@ from tensorflow import keras
 
 fashion_mnist = keras.datasets.fashion_mnist
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data() # loading the dataset
+# The dataset consists of 60000 training images and 10000 testing images
 
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'] # Clothing
+
 
 train_images = train_images/255.0
 test_images = test_images / 255.0
@@ -31,8 +34,9 @@ model.compile(
 ) # compilation of out neural network
 
 
-model.fit(train_images, train_labels, epochs = 3)
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose = 1)
+model.fit(train_images, train_labels, epochs = 3) # Training the model
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose = 1) # Evaluation
+
 print(test_acc)
 predictions = model.predict(test_images)
 print(class_names[np.argmax(predictions[56])]) # you change the index here
